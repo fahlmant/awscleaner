1af2dff5a560778ccab3ed74eaa8a59196b2db0f
agautam <amol_gautam25@yahoo.co.in>
2020-05-28 11:35:28 -0400

    code for credential test

---
+++ client.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	_ "context"
+	"fmt"
+	_ "fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+)
+
+const (
+	awsCredsSecretIDKey     = "AKIAUBEE5Y3BCPHVVKVV"
+	awsCredsSecretAccessKey = "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"
+)
+
+// Client is a wrapper object for actual AWS SDK clients to allow for easier testing.
+type Client interface {
+	//EC2
+	RunInstances(*ec2.RunInstancesInput) (*ec2.Reservation, error)
+	DescribeInstanceStatus(*ec2.DescribeInstanceStatusInput) (*ec2.DescribeInstanceStatusOutput, error)
+	TerminateInstances(*ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error)
+	DescribeVolumes(*ec2.DescribeVolumesInput) (*ec2.DescribeVolumesOutput, error)
+	DeleteVolume(*ec2.DeleteVolumeInput) (*ec2.DeleteVolumeOutput, error)
+	DescribeSnapshots(*ec2.DescribeSnapshotsInput) (*ec2.DescribeSnapshotsOutput, error)
+	DeleteSnapshot(*ec2.DeleteSnapshotInput) (*ec2.DeleteSnapshotOutput, error)
+	DescribeInstances(*ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error)
+}
+
+type awsClient struct {
+	ec2Client ec2iface.EC2API
+}
+
+// NewAwsClientInput input for new aws client
+type NewAwsClientInput struct {
+	AwsCredsSecretIDKey     string
+	AwsCredsSecretAccessKey string
+	AwsToken                string
+	AwsRegion               string
+	SecretName              string
+	NameSpace               string
+}
+
+func (c *awsClient) RunInstances(input *ec2.RunInstancesInput) (*ec2.Reservation, error) {
+	return c.ec2Client.RunInstances(input)
+}
+
+func (c *awsClient) DescribeInstanceStatus(input *ec2.DescribeInstanceStatusInput) (*ec2.DescribeInstanceStatusOutput, error) {
+	return c.ec2Client.DescribeInstanceStatus(input)
+}
+
+func (c *awsClient) TerminateInstances(input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	return c.ec2Client.TerminateInstances(input)
+}
+
+func (c *awsClient) DescribeVolumes(input *ec2.DescribeVolumesInput) (*ec2.DescribeVolumesOutput, error) {
+	return c.ec2Client.DescribeVolumes(input)
+}
+
+func (c *awsClient) DeleteVolume(input *ec2.DeleteVolumeInput) (*ec2.DeleteVolumeOutput, error) {
+	return c.ec2Client.DeleteVolume(input)
+}
+
+func (c *awsClient) DescribeSnapshots(input *ec2.DescribeSnapshotsInput) (*ec2.DescribeSnapshotsOutput, error) {
+	return c.ec2Client.DescribeSnapshots(input)
+}
+
+func (c *awsClient) DeleteSnapshot(input *ec2.DeleteSnapshotInput) (*ec2.DeleteSnapshotOutput, error) {
+	return c.ec2Client.DeleteSnapshot(input)
+}
+
+func (c *awsClient) DescribeInstances(input *ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
+	return c.ec2Client.DescribeInstances(input)
+}
+
+// NewClient creates our client wrapper object for the actual AWS clients we use.
+func NewClient(awsAccessID, awsAccessSecret, token, region string) (Client, error) {
+	awsConfig := &aws.Config{Region: aws.String(region)}
+	awsConfig.Credentials = credentials.NewStaticCredentials(
+		awsAccessID, awsAccessSecret, token)
+
+	s, err := session.NewSession(awsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &awsClient{
+		ec2Client: ec2.New(s),
+	}, nil
+}
+
+func main() {
+
+	client, err := NewClient("", "", "", "us-east-1")
+	fmt.Println(err)
+
+	result, err := client.DescribeInstances(&ec2.DescribeInstancesInput{})
+	fmt.Println(result)
+}
