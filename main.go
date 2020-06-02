1c40eeabbc1e7357c616f27d3f7d3b210e4c195d
agautam <amol_gautam25@yahoo.co.in>
2020-06-02 08:13:34 -0400

    change log : Enhancement : --- implemented pagination

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"awscleaner/pkg/aws"
+	clientpkg "awscleaner/pkg/aws"
 	"fmt"
 	_ "fmt"
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,26 +19,27 @@ func main() {
 
 	// creating a new cient with us-east-1 region by default
 	client, err := clientpkg.NewClient(accessID, secretKey, "", "us-east-1")
-	fmt.Println("client is %v", client)
+	//	fmt.Println("client is %v", client)
 	fmt.Println("error is %v", err)
 
 	// creating a caller identity
 	// jst for debugging purposes
-	calleridentity, err := client.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-	fmt.Println("calleridentity is ", calleridentity)
-	fmt.Println("the test account id is ", accountid)
+	//	calleridentity, err := client.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	//	fmt.Println("calleridentity is ", calleridentity)
+	//	fmt.Println("the test account id is ", accountid)
 
 	assumedRole, err := client.AssumeRole(&sts.AssumeRoleInput{RoleArn: aws.String("arn:aws:iam::173028519319:role/OrganizationAccountAccessRole"), RoleSessionName: aws.String("agautam")})
-	fmt.Println(assumedRole)
+	//	fmt.Println(assumedRole)
+	//	fmt.Println("error in assuming role is ",err);
 
 	assumedAccessKey := *assumedRole.Credentials.AccessKeyId
 	assumedSecretKey := *assumedRole.Credentials.SecretAccessKey
 	assumedSessionToken := *assumedRole.Credentials.SessionToken
 
 	// for debugging purpose only.
-	fmt.Println("new access id : ", assumedAccessKey)
-	fmt.Println("new secret key is", assumedSecretKey)
-	fmt.Println("new session token\n\n", assumedSessionToken)
+	//	fmt.Println("new access id : ", assumedAccessKey)
+	//	fmt.Println("new secret key is", assumedSecretKey)
+	//	fmt.Println("new session token\n\n", assumedSessionToken)
 
 	regionRange := []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "sa-east-1"}
 	for _, region := range regionRange {
@@ -51,14 +52,23 @@ func main() {
 		fmt.Println("error is %v", err)
 
 		// creating a caller identity
-		calleridentity2, err := client2.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-		fmt.Println("new calleridentity is ", calleridentity2)
+		//	calleridentity2, err := client2.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+		//	fmt.Println("new calleridentity is ", calleridentity2)
 
 		// trying to describe instances , it will probably fail with a root account
-		result2, err := client2.DescribeInstances(&ec2.DescribeInstancesInput{})
-		fmt.Println("error is ", err)
-		fmt.Println("EC2 instances", result2)
+		token := ""
+		for {
+			result2, err := client2.DescribeInstances(&ec2.DescribeInstancesInput{NextToken: aws.String(token)})
+			if err != nil {
+				fmt.Println("error is ", err)
+			}
+			fmt.Println("EC2 instances", result2)
+			if result2.NextToken != nil {
+				token = *result2.NextToken
+			} else {
+				break
+			}
+		}
 	}
-	// something wrong here. no error but no EC2 instances printed
 
 }
